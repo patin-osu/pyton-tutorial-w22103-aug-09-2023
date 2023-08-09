93fd7c5cdc5cb429ab6f1fa39d82a4a68c3c94eb
Patin Muangjan <patin@patin.dev>
2023-08-09 06:57:46 +0000

    Add variable'ed print()

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+#Variable
+name = "Patin"
+surname = "Muangjan"
+no = "9"
+student_id = "38832"
+school = "Streesmutprakan"
+hobby = "Web development"
+
+#Normal print() command
 print("""ภฑิล
 เมืองจันทร์
 เลขที่ 9
 38832
 สตรีสทุทรปราการ
-เขียนเว็บ""")
+เขียนเว็บ
+------------""")
+
+#Variable'ed print()
+print(name)
+print(surname)
+print(no)
+print(student_id)
+print(school)
+print(hobby)
